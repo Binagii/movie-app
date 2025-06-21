ac8afa4f1ce4a09f1f4bc0863d885c0ecc8a834d
Binagii <132564216+Binagii@users.noreply.github.com>
2025-06-22 00:21:56 +0800

    changes

--- backend/models/Movie.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const MovieSchema = new mongoose.Schema({
-    title : String,
-    description : String,
-    image : String
-});
-
-module.exports = mongoose.model('Movie', MovieSchema);
